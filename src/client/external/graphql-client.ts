b36ad884f533484a70086d477c1019e6f0cf561d
Jerry Chang <therealjerrycan@gmail.com>
2021-10-06 22:58:55 -0700

    Fix process.env issue with esbuild

--- src/client/external/graphql-client.ts
+++ src/client/external/graphql-client.ts
@@ -3,7 +3,9 @@ import {
   graphqlEndpoint
 } from '@app/client/constants';
 
-const endpoint = process.env.CLIENT_GRAPHQL_ENDPOINT || graphqlEndpoint;
+// Need to update esbuild with replace plugin
+//const endpoint = process.env.CLIENT_GRAPHQL_ENDPOINT || graphqlEndpoint;
+const endpoint = graphqlEndpoint;
 
 const client = new GraphQLClient(endpoint);
 
